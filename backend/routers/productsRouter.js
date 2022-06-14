5eb178db2484364937d6ffc0ae9920fca48b97a9
Manishankar Vakta <manishankarvakta@gmail.com>
2022-06-15 00:39:53 +0600

    products by id, code, ean

--- backend/routers/productsRouter.js
+++ backend/routers/productsRouter.js
@@ -21,5 +21,29 @@ expressAsyncHandler(async (req, res)=>{
     res.send({products});
 }))
 
+// get Products by _id
+productsRouter.get('/:id',
+expressAsyncHandler(async (req, res)=>{
+    const id = req.params.id;
+    const products = await Products.find({"_id": id});
+    res.send({products});
+}))
+
+// get Products by ean
+productsRouter.get('/by-ean/:ean',
+expressAsyncHandler(async (req, res)=>{
+    const ean = req.params.ean;
+    const products = await Products.find({"ean": ean});
+    res.send({products});
+}))
+
+
+// get Products by ean
+productsRouter.get('/by-code/:code',
+expressAsyncHandler(async (req, res)=>{
+    const code = req.params.code;
+    const products = await Products.find({"article_code": code});
+    res.send({products});
+}))
 
 export default productsRouter;
